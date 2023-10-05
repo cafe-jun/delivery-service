3838b220f721e9b48d4bafde9e9767d0ca9e56c6
jsshin <tb25271@gmail.com>
2023-10-05 19:16:27 +0900

    [feat] producer 업데이트

--- service/api/src/main/java/org/delivery/api/common/rabbitmq/Producer.java
+++ service/api/src/main/java/org/delivery/api/common/rabbitmq/Producer.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class Producer {
-    private final RabbitTemplate rabbitTemplate;
-
-    public void producer(String exchange, String routeKey, Object object) {
-        rabbitTemplate.convertAndSend(exchange,routeKey,object);
-    }
+//    private final RabbitTemplate rabbitTemplate;
+//
+//    public void producer(String exchange, String routeKey, Object object) {
+//        rabbitTemplate.convertAndSend(exchange,routeKey,object);
+//    }
 }
--- service/api/src/main/java/org/delivery/api/domain/userorder/producer/UserOrderProducer.java
+++ service/api/src/main/java/org/delivery/api/domain/userorder/producer/UserOrderProducer.java
@@ -10,18 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserOrderProducer {
 
-    private final Producer producer;
-
-    private static final String EXCHANGE = "delivery.exchange";
-    private static final String ROUTE_KEY = "delivery.key";
-
-    public void sendOrder(UserOrderEntity userOrderEntity){
-        sendOrder(userOrderEntity.getId());
-    }
-    public void sendOrder (Long userOrderId) {
-        var message = UserOrderMessage.builder()
-                .userOrderId(userOrderId)
-                .build();
-        producer.producer(EXCHANGE,ROUTE_KEY,message);
-    }
+//    private final Producer producer;
+//
+//    private static final String EXCHANGE = "delivery.exchange";
+//    private static final String ROUTE_KEY = "delivery.key";
+//
+//    public void sendOrder(UserOrderEntity userOrderEntity){
+//        sendOrder(userOrderEntity.getId());
+//    }
+//    public void sendOrder (Long userOrderId) {
+//        var message = UserOrderMessage.builder()
+//                .userOrderId(userOrderId)
+//                .build();
+//        producer.producer(EXCHANGE,ROUTE_KEY,message);
+//    }
 }
